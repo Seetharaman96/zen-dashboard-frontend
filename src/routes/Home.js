93511f85cde3eb2dc926f58b1a472494370c4bfe
Seetharaman96 <seetharam.ad96@gmail.com>
2023-06-23 12:39:14 +0530

    upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import HeroImg from '../components/HeroImg';
-import HomeDetails from '../components/HomeDetails';
+// import HomeDetails from '../components/HomeDetails';
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <HeroImg heading="ZenClass Dashboard" text="Hello all, welcome to zenclass dashboard" />
-        <HomeDetails />
+        {/* <HomeDetails /> */}
     </div>
   )
 }
